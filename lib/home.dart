2c1a1cfb8e63d9a3b0d48136331ecfb343a40c80
Yasmeen-Saad <yasmeensaad626@gmail.com>
2025-03-06 10:20:32 +0200

    lab day 4

--- lib/home.dart
+++ lib/home.dart
@@ -13,22 +13,53 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.menu),
-        title: Text("You clicked $counter times"),
-        centerTitle: true,
-        actions: [
-          IconButton(
-              onPressed: (){
-                setState(() {
-                  counter++;
-                });
-              },
-              icon: Icon(Icons.ads_click)
+      // appBar: AppBar(
+      //   leading: Icon(Icons.menu),
+      //   title: Text("Clicked $counter times"),
+      //   centerTitle: true,
+      //   actions: [
+      //     IconButton(
+      //         onPressed: (){
+      //           setState(() {
+      //             counter++;
+      //           });
+      //         },
+      //         icon: Icon(Icons.ads_click)
+      //     )
+      //   ],
+      // ),
+      backgroundColor: Colors.blueAccent,
+      body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 70,
+                color: Colors.lime,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 90,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: 90,
+                    height: 100,
+                    color: Colors.amber,
+                  ),
+                ],
+              ),
+              Container(
+                width: 70,
+                color: Colors.green,
+              ),
+            ],
           )
-        ],
       ),
     );
   }
 }
-
